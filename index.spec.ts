b968d4f6f65efda47f08d6b4f0e93237f3520c81
Ben Alt <ben.r.alt@gmail.com>
2024-06-29 19:23:27 -0500

    intital add, without tests

---
+++ index.spec.ts
@@ -0,0 +1,23 @@
+describe("isGameOver", () => {
+  it("runs a test", () => {
+    expect(true).toBe(true);
+  });
+});
+
+describe("applyTurnGameState", () => {
+  it("runs a test", () => {
+    expect(true).toBe(true);
+  });
+});
+
+describe("playerCanMakeMove", () => {
+  it("runs a test", () => {
+    expect(true).toBe(true);
+  });
+});
+
+describe("newMancalaGameState", () => {
+  it("runs a test", () => {
+    expect(true).toBe(true);
+  });
+});
